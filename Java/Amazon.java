c57ef3a29e5325f53433924445b50559d70abe25
Kaushal <kaushal.beyblader@gmail.com>
2021-02-07 17:44:45 -0800

    Update Amazon.java

--- Java/Amazon.java
+++ Java/Amazon.java
@@ -209,7 +209,30 @@ public class Amazon {
             data = d;
         }
     }
-	
-	
+	// Is the robot path a circle
+	public boolean isRobotBounded(String instructions) {
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        for(int i = 0; i < instructions.length(); i++) {
+            char ch = instructions.charAt(i);
+            if(ch == 'L') {
+                dir = (dir + 3) % 4;
+            } else if(ch =='R') {
+                dir = (dir + 1) % 4;
+            } else {
+                if(dir == 0) {
+                    y++;
+                } else if(dir == 1) {
+                    x++;
+                } else if(dir == 2) {
+                    y--;
+                } else {
+                    x--;
+                }
+            }
+        }
+        return (x == 0 && y == 0) || (dir != 0);
+    }
 	
 }
